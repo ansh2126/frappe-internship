728003e8f2d6197a29348ca5e569f41ef4af13dc
Ansh Sheradiya <anshsheradiya@gmail.com>
2026-08-17 10:01:56 +0000

    Add Python tuples example

---
+++ day4-python-basics/05_tuples.py
@@ -0,0 +1,9 @@
+student = ("Ansh", 20, "B.Sc IT", "Ahmedabad")
+
+print("Student:", student)
+print("Name:", student[0])
+print("Age:", student[1])
+print("Course:", student[2])
+print("City:", student[3])
+print("Total values:", len(student))
+
