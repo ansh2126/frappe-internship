b68021b5e14a54b19b8c93f67f40ba7b869e7b7e
Ansh Sheradiya <anshsheradiya@gmail.com>
2026-08-17 09:08:12 +0000

    Add Python data types example

---
+++ day3-python-basics/01_datatypes.py
@@ -0,0 +1,13 @@
+name = "Ansh"
+age = 20
+height = 5.9
+is_student = True
+
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
