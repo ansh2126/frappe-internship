ef7da9b6972ab97d6b4877125a973e0b096798d3
Ansh Sheradiya <anshsheradiya@gmail.com>
2026-08-17 10:00:12 +0000

    Add Python dictionaries example

---
+++ day4-python-basics/03_dictionaries.py
@@ -0,0 +1,13 @@
+student = {
+    "name": "Ansh",
+    "age": 20,
+    "course": "B.Sc IT"
+}
+
+print("Student:", student)
+print("Name:", student["name"])
+print("Age:", student["age"])
+
+student["city"] = "Ahmedabad"
+print("After adding city:", student)
+
