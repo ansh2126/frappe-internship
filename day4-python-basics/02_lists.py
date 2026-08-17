3ebb0641b2b6a02b6355f50e7f56d328cf7b67a5
Ansh Sheradiya <anshsheradiya@gmail.com>
2026-08-17 09:58:29 +0000

    Add Python lists example

---
+++ day4-python-basics/02_lists.py
@@ -0,0 +1,13 @@
+students = ["Ansh", "Rahul", "Jay"]
+
+print("Students:", students)
+
+students.append("Amit")
+print("After adding:", students)
+
+students.remove("Rahul")
+print("After removing:", students)
+
+print("First student:", students[0])
+print("Total students:", len(students))
+
