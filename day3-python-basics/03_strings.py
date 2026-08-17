05668346158144e98aeab20ecfa22a8b30ff3a21
Ansh Sheradiya <anshsheradiya@gmail.com>
2026-08-17 09:42:35 +0000

    Add Python strings example

---
+++ day3-python-basics/03_strings.py
@@ -0,0 +1,9 @@
+first_name = "Ansh"
+last_name = "Sheradiya"
+
+full_name = first_name + " " + last_name
+
+print("Full Name:", full_name)
+print("Length:", len(full_name))
+print("Uppercase:", full_name.upper())
+print("Lowercase:", full_name.lower())
